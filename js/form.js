862587f27121c8dcc4b39acbecda70d481ccb2c0
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-07-15 21:19:42 +0530

    added: validating user email using regex

--- js/form.js
+++ js/form.js
@@ -16,3 +16,20 @@ text.addEventListener('input', function(){
     }
 });
 
+// email validation
+
+const newEmail = document.querySelector("#email");
+const emailError = document.querySelector('.email-error');
+newEmail.addEventListener('input', function(){
+    let emailRegex = RegExp('^[a-zA-z0-9.+-]+[@]+[a-zA-z0-9]+[.]+[a-zA-z0-9.]{2,}$');
+    if(newEmail.value.length == 0 ){
+        emailError.textContent = "Email is incorrect";
+    }else{
+        if (emailRegex.test(newEmail.value)){
+            emailError.textContent = "";
+        }
+        else {
+            emailError.textContent = "Email is incorrect";
+        }
+    }
+});
